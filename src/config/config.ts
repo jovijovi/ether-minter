22ca93476bd08712d406649f2e8fc5af42af8f0f
jovijovi <mageyul@hotmail.com>
2022-05-27 11:52:02 +0800

    feat: update config

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,8 +6,31 @@ export namespace customConfig {
 		confirmations: number
 	}
 
+	export class Minter {
+		address: string
+		keyStoreSK?: string
+	}
+
+	class Mint {
+		contractAddress: string
+		gasPrice: {
+			upper: {
+				threshold: string
+				callback: string
+			},
+			lower: {
+				threshold: string
+				callback: string
+			},
+		}
+		apiResponseCode: any
+		randomMinter: boolean
+		minterList: Minter[]
+	}
+
 	export class CustomConfig {
 		tx: TxConfig
+		mint: Mint
 	}
 
 	let customConfig: CustomConfig;
@@ -23,5 +46,23 @@ export namespace customConfig {
 	// GetTxConfig returns tx config
 	export function GetTxConfig(): TxConfig {
 		return customConfig.tx;
+	}
+
+	// GetMint returns mint config
+	export function GetMint(): Mint {
+		if (customConfig.mint) {
+			return customConfig.mint;
+		}
+
+		throw new Error(`GetMint Failed, invalid config`);
+	}
+
+	// GetMintRspCode returns mint response code
+	export function GetMintRspCode(): any {
+		if (customConfig.mint) {
+			return customConfig.mint.apiResponseCode;
+		}
+
+		throw new Error(`GetMintRspCode Failed, invalid config`);
 	}
 }
