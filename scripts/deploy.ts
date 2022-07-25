76614d65b88e65dabddd3a997d0beae4335910c3
jovijovi <mageyul@hotmail.com>
2022-07-25 17:28:43 +0800

    test: revert to 26450cb7

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,11 @@
-import {ethers, upgrades} from 'hardhat';
+import {ethers} from 'hardhat';
 import {env} from 'process';
-import {Avatar__factory} from '../typechain-types';
+import {Avatar, Avatar__factory} from '../typechain-types';
 
-// Params for contract
+// Params for immutable contract
 // (default params used for test only)
 const params = {
 	ContractName: 'Avatar',
-	ContractInitializer: '__Avatar_init',
 	NFTName: env.NFT_NAME ? env.NFT_NAME : 'AvatarNFT',
 	NFTSymbol: env.NFT_SYMBOL ? env.NFT_SYMBOL : 'AvatarNFTSymbol',
 	NFTBaseTokenURI: env.NFT_BASETOKENURI ? env.NFT_BASETOKENURI : 'https://localhost:2022/nft/',
@@ -15,9 +14,7 @@ const params = {
 
 async function main(): Promise<void> {
 	const factory: Avatar__factory = await ethers.getContractFactory(params.ContractName) as Avatar__factory;
-	const contract = await upgrades.deployProxy(factory,
-		[params.NFTName, params.NFTSymbol, params.NFTBaseTokenURI, params.MaxSupply, []],
-		{initializer: params.ContractInitializer});
+	const contract: Avatar = await factory.deploy(params.NFTName, params.NFTSymbol, params.NFTBaseTokenURI, params.MaxSupply, []);
 	await contract.deployed();
 	console.log("Contract deployed to: ", contract.address);
 }
