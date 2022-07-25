8c327694faa2668950fc6913cd1e0a42f6613eb1
jovijovi <mageyul@hotmail.com>
2022-07-25 17:15:01 +0800

    feat(contracts/AvatarUpgradeable): add AvatarUpgradeable contract

---
+++ contracts/AvatarUpgradeable/AvatarUpgradeable.sol
@@ -0,0 +1,440 @@
+// SPDX-License-Identifier: MIT
+/*
+ *   █████╗ ██╗   ██╗ █████╗ ████████╗ █████╗ ██████╗
+ *  ██╔══██╗██║   ██║██╔══██╗╚══██╔══╝██╔══██╗██╔══██╗
+ *  ███████║██║   ██║███████║   ██║   ███████║██████╔╝
+ *  ██╔══██║╚██╗ ██╔╝██╔══██║   ██║   ██╔══██║██╔══██╗
+ *  ██║  ██║ ╚████╔╝ ██║  ██║   ██║   ██║  ██║██║  ██║
+ *  ╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝
+ *
+ *  ██╗   ██╗██████╗  ██████╗ ██████╗  █████╗ ██████╗ ███████╗ █████╗ ██████╗ ██╗     ███████╗
+ *  ██║   ██║██╔══██╗██╔════╝ ██╔══██╗██╔══██╗██╔══██╗██╔════╝██╔══██╗██╔══██╗██║     ██╔════╝
+ *  ██║   ██║██████╔╝██║  ███╗██████╔╝███████║██║  ██║█████╗  ███████║██████╔╝██║     █████╗
+ *  ██║   ██║██╔═══╝ ██║   ██║██╔══██╗██╔══██║██║  ██║██╔══╝  ██╔══██║██╔══██╗██║     ██╔══╝
+ *  ╚██████╔╝██║     ╚██████╔╝██║  ██║██║  ██║██████╔╝███████╗██║  ██║██████╔╝███████╗███████╗
+ *   ╚═════╝ ╚═╝      ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝╚═════╝ ╚══════╝╚══════╝
+ *
+ *  AvatarUpgradeable Contract v0.4.0
+ *  Dependencies:
+ *  "@openzeppelin/contracts-upgradeable": "4.7.1"
+ *  "erc721a-upgradeable": "4.2.1"
+ */
+
+pragma solidity ^0.8.4;
+
+import "erc721a-upgradeable/contracts/ERC721AUpgradeable.sol";
+import "erc721a-upgradeable/contracts/extensions/ERC721AQueryableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "./PermissionUpgradeable.sol";
+
+/**
+ * @title An avatar NFT contract.
+ */
+contract AvatarUpgradeable is ERC721AUpgradeable, ERC721AQueryableUpgradeable, ReentrancyGuardUpgradeable, OwnableUpgradeable, PermissionControlUpgradeable {
+    /* *******
+     * Globals
+     * *******
+     */
+
+    /**
+     * @dev Maximum supply.
+     */
+    uint256 public maxSupply;
+
+    /**
+     * @dev Finalization state, only false can permit mint.
+     */
+    bool public finalization;
+
+    /**
+     * @dev A URL for the storefront-level metadata for contract.
+     */
+    string private _contractURI;
+
+    /**
+     * @dev Base URI for computing {tokenURI}.
+     */
+    string private _baseTokenURI;
+
+    /**
+     * @dev Mapping from token id to sha256 hash of content.
+     * (tokenId <-> avatar content hash)
+     */
+    mapping(uint256 => bytes32) public tokenContentHashes;
+
+    /**
+     * @dev Mapping from contentHash to tokenID.
+     * (avatar content hash <-> tokenId)
+     */
+    mapping(bytes32 => uint256) private _contentHashes;
+
+    /* *********
+     * Modifiers
+     * *********
+     */
+
+    /**
+     * @notice Require that the token has not been burned and has been minted
+     */
+    modifier onlyExistingToken(uint256 tokenId) {
+        require(_exists(tokenId), "Avatar: nonexistent token");
+        _;
+    }
+
+    /**
+     * @notice Ensure that the provided spender is the approved or the owner of
+     * the avatar for the specified tokenId
+     */
+    modifier onlyApprovedOrOwner(address spender, uint256 tokenId) {
+        require(_isApprovedOrOwner(spender, tokenId), "Avatar: only approved or owner");
+        _;
+    }
+
+    /**
+     * @notice Ensure that the collection of this contract is finalization, and can not mint any more.
+     */
+    modifier onlyMintAvailable() {
+        require(finalization == false, "Avatar: mint function is not available.");
+        _;
+    }
+
+    /**
+     * @notice Ensure that within the maximum supply limit.
+     */
+    modifier onlyWithinMaxSupply(uint256 quantity) {
+        require(quantity + totalSupply() <= maxSupply, "Avatar: reach the maximum supply limit");
+        _;
+    }
+
+    /**
+     * @notice Ensure that the hash of contentHashList is not empty and the token with the content hash is not minted.
+     */
+    modifier onlyValidContentHashList(bytes32[] memory contentHashList) {
+        require(contentHashList.length > 0, "Avatar: contentHashList is empty");
+        for (uint256 i = 0; i < contentHashList.length; i++) {
+            bytes32 contentHash = contentHashList[i];
+            require(contentHash != 0, "Avatar: content hash must be non-zero");
+            require(_contentHashes[contentHash] == 0, "Avatar: a token has already been minted with this content hash");
+        }
+        _;
+    }
+
+    /**
+     * @dev Initializes the contract by setting `name`, `symbol`, `baseTokenURI`, `maxSupply` and `operators` to the token collection.
+     * `operators` is optional.
+     * `initializerERC721A` for `ERC721AUpgradeable`.
+     * `initializer` for OpenZeppelin's `OwnableUpgradeable`.
+     */
+    function __Avatar_init(string memory name_, string memory symbol_, string memory baseTokenURI_, uint256 maxSupply_,
+        address[] memory operators) public initializerERC721A initializer {
+        __Avatar_init_unchained(name_, symbol_, baseTokenURI_, maxSupply_, operators);
+    }
+
+    function __Avatar_init_unchained(string memory name_, string memory symbol_, string memory baseTokenURI_, uint256 maxSupply_,
+        address[] memory operators) public initializerERC721A initializer {
+        require(maxSupply_ > 0, "Avatar: invalid maxSupply");
+
+        __ERC721A_init(name_, symbol_);
+        __Ownable_init();
+        __PermissionControl_init();
+
+        _baseTokenURI = baseTokenURI_;
+        maxSupply = maxSupply_;
+        finalization = false;
+        if (operators.length > 0) {
+            addOperators(operators);
+        }
+    }
+
+    /* ****************
+     * Public Functions
+     * ****************
+     */
+
+    /**
+     * @notice A URL for the storefront-level metadata for contract.
+     * @dev Ref: https://docs.opensea.io/docs/contract-level-metadata
+     */
+    function contractURI() public view returns (string memory) {
+        return _contractURI;
+    }
+
+    /**
+     * @dev Returns whether `tokenId` exists.
+     */
+    function exists(uint256 tokenId) public view returns (bool) {
+        return _exists(tokenId);
+    }
+
+    /**
+     * @dev Returns the total amount of tokens minted in the contract.
+     */
+    function totalMinted() public view returns (uint256) {
+        return _totalMinted();
+    }
+
+    /**
+     * @dev Returns the total number of tokens burned.
+     */
+    function totalBurned() public view returns (uint256) {
+        return _totalBurned();
+    }
+
+    /**
+     * @notice get the tokenId by the contentHash if the token had been minted
+     * @return the tokenId
+     */
+    function getTokenIdByContentHash(bytes32 contentHash) public view returns (uint256)
+    {
+        require(_contentHashes[contentHash] > 0, "Avatar: the token has not been minted.");
+        return _contentHashes[contentHash];
+    }
+
+    /**
+     * @dev Returns whether "contentHash" has ever existed.
+     */
+    function contentHashExists(bytes32 contentHash) public view returns (bool)
+    {
+        return _contentHashes[contentHash] > 0;
+    }
+
+    /**
+     * @dev Returns all content hash.
+     */
+    function getAllContentHash() public view returns (bytes32[] memory)
+    {
+        uint256 length = _nextTokenId() - 1 >= 0 ? _nextTokenId() - 1 : 0;
+        bytes32[] memory allContentHash = new bytes32[](length);
+
+        for (uint256 i = _startTokenId(); i <= length; i++) {
+            allContentHash[i - 1] = tokenContentHashes[i];
+        }
+
+        return allContentHash;
+    }
+
+    /* ******************
+     * External Functions
+     * ******************
+     */
+
+    /**
+     * @dev Change contractURI.
+     */
+    function changeContractURI(string memory contractURI_) external
+    onlyOwner
+    {
+        _contractURI = contractURI_;
+    }
+
+    /**
+     * @dev Set baseTokenURI.
+     */
+    function setBaseTokenURI(string memory baseTokenURI_) external
+    onlyOwner
+    {
+        _baseTokenURI = baseTokenURI_;
+    }
+
+    /**
+     * @dev Set max supply.
+     */
+    function setMaxSupply(uint256 val) external
+    onlyOwner
+    {
+        maxSupply = val;
+    }
+
+    /**
+     * @dev Mint to message sender.
+     */
+    function mint(uint256 quantity) external
+    nonReentrant
+    {
+        _mintTo(_msgSender(), quantity);
+    }
+
+    /**
+     * @dev Mint to specified address.
+     */
+    function mintTo(address to, uint256 quantity) external
+    nonReentrant
+    {
+        _mintTo(to, quantity);
+    }
+
+    /**
+     * @dev Mint to specified address with content hash list.
+     */
+    function mintForCreator(address to, uint256 quantity, bytes32[] memory contentHashList) external
+    nonReentrant
+    {
+        _mintForCreator(to, quantity, contentHashList);
+    }
+
+    /**
+     * @dev Batch tokens transfer from 1 to 1.
+     */
+    function batchTransfer(address from, address to, uint256 fromTokenId, uint256 toTokenId) external
+    nonReentrant
+    {
+        _batchTransfer(from, to, fromTokenId, toTokenId);
+    }
+
+    /**
+     * @dev Batch tokens transfer from 1 to N.
+     */
+    function batchTransferToN(address from, address[] memory to, uint256[] memory tokenIds) external
+    nonReentrant
+    {
+        _batchTransferToN(from, to, tokenIds);
+    }
+
+    /**
+     * @dev Burn a token.
+     */
+    function burn(uint256 tokenId) external
+    nonReentrant
+    onlyExistingToken(tokenId)
+    onlyApprovedOrOwner(_msgSender(), tokenId)
+    {
+        _burn(tokenId);
+    }
+
+    /**
+     * @dev Batch tokens burn.
+     */
+    function batchBurn(uint256 fromTokenId, uint256 toTokenId) external
+    nonReentrant
+    {
+        _batchBurn(fromTokenId, toTokenId);
+    }
+
+    /**
+     * @notice Finalize this contract and can not mint any more.
+     */
+    function finalize() external
+    onlyOwner
+    {
+        require(finalization == false, "Avatar: already finalized");
+        finalization = true;
+    }
+
+    /* ******************
+     * Internal Functions
+     * ******************
+     */
+
+    /**
+     * @notice Mints a new token for `creator`. Its token ID will be automatically
+     * assigned (and available on the emitted {IERC721-Transfer} event), and the token
+     * URI autogenerated based on the base URI passed at construction.
+     *
+     * See {ERC721-_safeMint}.
+     *
+     * On mint, also set the sha256 hashes of the content for integrity
+     * checks, along with the initial URIs to point to the content. Attribute
+     * the token ID to the To, mark the content hash as used.
+     *
+     * Note that the content hash must be unique for future mints to prevent duplicate avatar.
+     */
+    function _mintTo(address to, uint256 quantity) internal
+    onlyMintAvailable
+    onlyOperator
+    onlyWithinMaxSupply(quantity)
+    {
+        _safeMint(to, quantity);
+    }
+
+    /**
+     * @dev Mint to specified address with content hash list.
+     */
+    function _mintForCreator(address to, uint256 quantity, bytes32[] memory contentHashList) internal
+    onlyMintAvailable
+    onlyOperator
+    onlyWithinMaxSupply(quantity)
+    onlyValidContentHashList(contentHashList)
+    {
+        require(contentHashList.length == quantity, "Avatar: quantity and contentHashList length mismatch");
+
+        uint256 startTokenId = _nextTokenId();
+
+        _safeMint(to, quantity);
+
+        for (uint256 i = 0; i < contentHashList.length; i++) {
+            uint256 tokenId = startTokenId + i;
+            bytes32 contentHash = contentHashList[i];
+            _setTokenContentHash(tokenId, contentHash);
+            _contentHashes[contentHash] = tokenId;
+        }
+    }
+
+    /**
+     * @dev Returns the starting token ID.
+     */
+    function _startTokenId() internal view override virtual returns (uint256) {
+        return 1;
+    }
+
+    /**
+     * @dev Base URI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`. Empty
+     * by default, can be overridden in child contracts.
+     */
+    function _baseURI() internal view override virtual returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    /**
+     * @dev Returns whether `spender` is allowed to manage `tokenId`.
+     *
+     * Requirements:
+     *
+     * - `tokenId` must exist.
+     */
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+        require(_exists(tokenId), "Avatar: operator query for nonexistent token");
+        address owner = ownerOf(tokenId);
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    /**
+     * @dev Set token content hash.
+     */
+    function _setTokenContentHash(uint256 tokenId, bytes32 contentHash) internal virtual
+    onlyExistingToken(tokenId)
+    {
+        tokenContentHashes[tokenId] = contentHash;
+    }
+
+    /**
+     * @dev Batch tokens transfer from 1 to 1.
+     */
+    function _batchTransfer(address from, address to, uint256 fromTokenId, uint256 toTokenId) internal virtual {
+        require(toTokenId > fromTokenId, "Avatar: invalid tokenId");
+        for (uint256 tokenId = fromTokenId; tokenId <= toTokenId; tokenId++) {
+            safeTransferFrom(from, to, tokenId);
+        }
+    }
+
+    /**
+     * @dev Batch tokens transfer from 1 to N.
+     */
+    function _batchTransferToN(address from, address[] memory to, uint256[] memory tokenIds) internal virtual {
+        require(to.length == tokenIds.length, "Avatar: invalid length of tokenIds");
+        for (uint256 i = 0; i < to.length; i++) {
+            safeTransferFrom(from, to[i], tokenIds[i]);
+        }
+    }
+
+    /**
+     * @dev Batch tokens burn.
+     */
+    function _batchBurn(uint256 fromTokenId, uint256 toTokenId) internal virtual {
+        require(toTokenId > fromTokenId, "Avatar: invalid tokenId");
+        for (uint256 tokenId = fromTokenId; tokenId <= toTokenId; tokenId++) {
+            _burn(tokenId, true);
+        }
+    }
+}
---
+++ contracts/AvatarUpgradeable/PermissionUpgradeable.sol
@@ -0,0 +1,138 @@
+// SPDX-License-Identifier: MIT
+// PermissionControlUpgradeable Contract v0.4.0
+pragma solidity ^0.8.4;
+
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+abstract contract PermissionControlUpgradeable is OwnableUpgradeable {
+    /**
+     * @dev Operator map.
+     */
+    mapping(address => bool) private _operatorMap;
+
+    /**
+     * @dev Operator list.
+     */
+    address[] private _operatorList;
+
+    /**
+     * @dev Emitted when add an operator.
+     */
+    event AddOperator(address indexed operator);
+
+    /**
+     * @dev Emitted when remove an operator.
+     */
+    event RemoveOperator(address indexed operator);
+
+    /**
+     * @dev Throws if called by any account other than the operator.
+     */
+    modifier onlyOperator() {
+        require(_operatorMap[_msgSender()], "PermissionControl: invalid operator");
+        _;
+    }
+
+    /**
+     * @dev Throws if the operator already exist.
+     */
+    modifier onlyOperatorNotExist(address operator) {
+        require(!_operatorMap[operator], "PermissionControl: operator already exist");
+        _;
+    }
+
+    /**
+     * @dev Throws if the operator not exist.
+     */
+    modifier onlyOperatorExist(address operator) {
+        require(_operatorMap[operator], "PermissionControl: operator not exist");
+        _;
+    }
+
+    /**
+     * @dev Initializes the contract setting default operator as the initial owner.
+     */
+    function __PermissionControl_init() public initializer {
+        addOperator(_msgSender());
+    }
+
+    /**
+     * @dev Get operator list.
+     */
+    function getOperatorList() public view returns (address[] memory) {
+        return _operatorList;
+    }
+
+    /**
+     * @dev Get operator list length.
+     */
+    function getOperatorListLength() public view returns (uint) {
+        return _operatorList.length;
+    }
+
+    /**
+     * @dev Check if it is the operator address.
+     */
+    function isOperator(address operator) public view returns (bool) {
+        return _operatorMap[operator];
+    }
+
+    /**
+     * @dev Add operator.
+     */
+    function addOperator(address operator) public onlyOwner onlyOperatorNotExist(operator) {
+        _operatorMap[operator] = true;
+        _operatorList.push(operator);
+
+        emit AddOperator(operator);
+    }
+
+    /**
+     * @dev Add operators. Skip if operator already exists.
+     */
+    function addOperators(address[] memory operators) public onlyOwner {
+        require(operators.length > 0, "PermissionControl: no operators");
+        for (uint256 i = 0; i < operators.length; i++) {
+            if (_operatorMap[operators[i]]) {
+                continue;
+            }
+            _operatorMap[operators[i]] = true;
+            _operatorList.push(operators[i]);
+            emit AddOperator(operators[i]);
+        }
+    }
+
+    /**
+     * @dev Remove operator.
+     */
+    function removeOperator(address operator) public onlyOwner onlyOperatorExist(operator) {
+        _operatorMap[operator] = false;
+        _removeOperatorFromList(operator);
+
+        emit RemoveOperator(operator);
+    }
+
+    /**
+     * @dev Operator retire.
+     */
+    function retire() public onlyOperator {
+        _operatorMap[_msgSender()] = false;
+        _removeOperatorFromList(_msgSender());
+
+        emit RemoveOperator(_msgSender());
+    }
+
+    /**
+     * @dev Remove operator from list.
+     */
+    function _removeOperatorFromList(address operator) private {
+        for (uint256 i = 0; i < _operatorList.length; i++) {
+            if (_operatorList[i] == operator) {
+                address last = _operatorList[_operatorList.length - 1];
+                _operatorList[i] = last;
+                _operatorList.pop();
+                break;
+            }
+        }
+    }
+}
